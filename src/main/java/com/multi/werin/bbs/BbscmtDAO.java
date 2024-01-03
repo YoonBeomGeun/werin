0da38d53dcebbddd35a786e7cc8bd41b36ea9e49
sjh <sjh2675@naver.com>
2024-01-03 11:49:02 +0900

    점심

--- src/main/java/com/multi/werin/bbs/BbscmtDAO.java
+++ src/main/java/com/multi/werin/bbs/BbscmtDAO.java
@@ -8,25 +8,7 @@ import org.springframework.stereotype.Repository;
 
 
 
-@Repository
 public class BbscmtDAO {
 
-	@Autowired
-	SqlSessionTemplate my;
 	
-	public int insert(BbscmtVO vo) {
-		return my.insert("bbscmt.create",vo);
-	}
-	
-	public List<BbscmtVO> list(int id) {
-		return my.selectList("bbscmt.list", id);
-	}
-	
-	public int update(BbscmtVO vo) {
-	    return my.update("bbscmt.update", vo);
-	}
-	
-	 public int delete(BbscmtVO vo) {
-	     return my.delete("bbscmt.delete", vo);
-	 }
 }
