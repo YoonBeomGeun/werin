df0ce1affdad1b9d676ee12144d229e30dcfc66d
sjh <sjh2675@naver.com>
2024-01-10 16:01:28 +0900

    검색 아직구현중

--- src/main/java/com/multi/werin/bbs/BbsController.java
+++ src/main/java/com/multi/werin/bbs/BbsController.java
@@ -1,10 +1,15 @@
 package com.multi.werin.bbs;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpSession;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -100,37 +105,57 @@ public class BbsController{
 //	@RequestMapping("bbs/getSearchList")
 //	@ResponseBody
 //	public String getSearchList(PageVO pageVO, Model model) {
-//	    pageVO.setPage(1);
 //		pageVO.setStartEnd();
+//		
 //	    System.out.println("getSerchList");
 //	    System.out.println("pageVO  "+ pageVO);
 //	    List<BbsVO> result = dao.getSearchList(pageVO);
 //	    System.out.println("result.size()"+ result.size());
-//	    int count= 
 //	    
-//	    model.addAttribute("list", result);
-//	    model.addAttribute("pages", pages);
-//	    model.addAttribute("count", count);
-//	    return "bbs/bbs0";  // bbs0.jsp로 forward
-	
-	
-	
-//		pageVO.setStartEnd();// start, end계산해주는 메서드
-//		System.out.println(pageVO);
-//		List<BbsVO> list = dao.(pageVO);
-//		// 전체 페이지수 구하기
-//		int count = dao.count(); // 전체게시물 수
-//		System.out.println("전체 게시물수>> " + count);
-//		int pages = count / 10;
+//	    int count=  dao.count2(); // 전체게시물 수 
+//	    int pages = count / 10;
 //		if (count % 10 != 0) {
 //			pages = count / 10 + 1;
 //		}
-//		// 모델로 views까지 페이지수를 넘겨야 페이지수 버튼을 만들 수 있음.
-//		System.out.println("페이지당 가지고 온 게시물수>> " + list.size());
-//		// views의 list1.jsp로 전달
+//	    model.addAttribute("list", result);
+//	    model.addAttribute("pages", pages);
+//	    model.addAttribute("count", count);
+//	    model.addAttribute("keyword", pageVO.getKeyword());
+//	    model.addAttribute("type", pageVO.getType());
+//	    return "bbs/bbs";  
+//	
+//	}
+	
+	
+//	@RequestMapping("bbs/getSearchList")
+//	@ResponseBody
+//	public Map<String, Object> getSearchList(PageVO pageVO, Model model) {
+//	    Map<String, Object> response = new HashMap<>();
+//
+//	    pageVO.setStartEnd();
+//	    List<BbsVO> result = dao.getSearchList(pageVO);
+//	    int count = dao.count2(); // 전체게시물 수
+//	    int pages = count / 10;
+//	    if (count % 10 != 0) {
+//	        pages = count / 10 + 1;
+//	    }
+//
+//	    response.put("list", result);
+//	    response.put("pages", pages);
+//	    response.put("count", count);
+//	    response.put("keyword", pageVO.getKeyword());
+//	    response.put("type", pageVO.getType());
+//
+//	    return  response;
+//	}
+//	
+//	@RequestMapping("bbs/bbs00")
+//	@ResponseBody
+//	public String list3(PageVO pageVO, Model model) {
+//		pageVO.setStartEnd();
+//		List<BbsVO> list = dao.list2(pageVO);
 //		model.addAttribute("list", list);
-//		model.addAttribute("pages", pages);
-//		model.addAttribute("count", count);
+//		return "bbs/bbs"; 
 //	}
 
 	
