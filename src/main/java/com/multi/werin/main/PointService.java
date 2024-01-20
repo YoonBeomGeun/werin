1bbe71df17b8f759ba02937c429a924af65748b5
haneuling <skytravel_er@naver.com>
2024-01-20 18:00:59 +0900

    일정 리스트 뽑아오기 해야함

--- src/main/java/com/multi/werin/main/PointService.java
+++ src/main/java/com/multi/werin/main/PointService.java
@@ -1,37 +1,27 @@
-package com.multi.werin.main;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.multi.werin.member.MemberDAO;
-import com.multi.werin.member.MemberVO;
-
-@Service
-public class PointService{
-
-	@Autowired
-	MemberDAO dao;
-	
-	public void userpoint(int variation, MemberVO memberVO) {
-		
-		int currentpoint = 0;
-		int totalpoint = 0;
-		String membergrade = "";
-		
-		MemberVO getinfo = dao.pointcheck(memberVO);
-		currentpoint = getinfo.getMember_level();
-		totalpoint = currentpoint+variation;
-		dao.updatelevel(totalpoint); //정산된 포인트를 다시 업데이트
-		
-		if(totalpoint<10) {
-			membergrade = "bronze";
-		}else if(totalpoint >= 10 && totalpoint < 20) {			
-			membergrade = "silver";
-		}else {			
-			membergrade = "gold";
-		}
-		
-		//굳이 저장을 해야하나? 저장이 잘 됐는지 확인하기위해 if문으로 확인해보는게 좋은가...?
-		dao.updategrade(memberVO); //등급을 다시 확인후 업데이트
-	}
-	
-}
+/*
+ * package com.multi.werin.main;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.stereotype.Service; import
+ * com.multi.werin.member.MemberDAO; import com.multi.werin.member.MemberVO;
+ * 
+ * @Service public class PointService{
+ * 
+ * @Autowired MemberDAO dao;
+ * 
+ * public void userpoint(int variation, MemberVO memberVO) {
+ * 
+ * int currentpoint = 0; int totalpoint = 0; String membergrade = "";
+ * 
+ * MemberVO getinfo = dao.pointcheck(memberVO); currentpoint =
+ * getinfo.getMember_level(); totalpoint = currentpoint+variation;
+ * dao.updatelevel(totalpoint); //정산된 포인트를 다시 업데이트
+ * 
+ * if(totalpoint<10) { membergrade = "bronze"; }else if(totalpoint >= 10 &&
+ * totalpoint < 20) { membergrade = "silver"; }else { membergrade = "gold"; }
+ * 
+ * //굳이 저장을 해야하나? 저장이 잘 됐는지 확인하기위해 if문으로 확인해보는게 좋은가...?
+ * dao.updategrade(memberVO); //등급을 다시 확인후 업데이트 }
+ * 
+ * }
+ */
